da3b8234d7f7beec73c654bdef6a07354c05dbc0
Adam Skwersky <adam.skwersky@hcl.com>
2018-04-30 13:43:11 -0400

    update package-lock for security issue

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. Note - dashboard gadgets won't reload.
         </p>
       </div>
     );
